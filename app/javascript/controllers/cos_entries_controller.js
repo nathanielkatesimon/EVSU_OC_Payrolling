6d07b64a78732e11d6ed2e1ea9413a12cacfd5f0
justin <arinojustine4@gmail.com>
2024-06-04 11:26:21 +0800

    Update COS

--- app/javascript/controllers/cos_entries_controller.js
+++ app/javascript/controllers/cos_entries_controller.js
@@ -7,21 +7,21 @@ export default class extends Controller {
   calculate(event) {
     clearTimeout(this.interval)
     this.interval = setTimeout(async () => {
-
-
-
-      let { authenticityToken, id } = event.target.dataset
+      let { authenticityToken, id, value } = event.target.dataset
       let csrfToken = document.querySelector('meta[name="csrf-token"]').content
+      
+      let data = { 'authenticity_token': authenticityToken, "cos_entry": {} }
+      data["cos_entry"][value] = event.target.value
 
-      let data = { 'authenticity_token': authenticityToken, 'absences': (event.target.value || 0) }
 
-      let res = await fetch("/regular_entries/calculate/" + id, { method: "PATCH", body: JSON.stringify(data), headers: { "Content-Type": "application/json", "X-CSRF-Token": csrfToken } })
+      let res = await fetch("/cos_entries/calculate/" + id, { method: "PATCH", body: JSON.stringify(data), headers: { "Content-Type": "application/json", "X-CSRF-Token": csrfToken } })
       let res_json = await res.json()
 
       if (res_json.success) {
         this.grossTarget.innerText = res_json.gross
         this.netTarget.innerText = res_json.net
-        this.absentsTarget.innerText = res_json.absents
+        this.undertimeTarget.innerText = res_json.undertime
+        this.overtimeTarget.innerText = res_json.overtime
       }
     }, 300)
   }
